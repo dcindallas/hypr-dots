f1626533ca2db7ef233b669d8ee1ac1f7c1367bd
Duane Cummins <duane.cummins@gmail.com>
2026-02-04 16:42:44 -0600

    Rice Sync Update - 2026-02-04

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,7 +6,7 @@ end
 alias config='/usr/bin/git --git-dir=/home/duane/hypr-dots/ --work-tree=/home/duane'
 
 # 1. Define and EXPORT the path first
-set -gx STARSHIP_CONFIG /home/duane/.config/starship/gruv.toml
+# set -gx STARSHIP_CONFIG /home/duane/.config/starship/duane.toml
 
 # 2. Then initialize Starship
-starship init fish | source
+# starship init fish | source
